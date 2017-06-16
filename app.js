2a6b40ae1b113c233f7cc9fc3010b8af81cad8ba
Ronda Wylie <rondawylie@yahoo.com>
2017-06-16 14:44:58 -0500

    Function get_weather working

--- app.js
+++ app.js
@@ -14,13 +14,20 @@ app.get('/', function (request, response) {
   response.render('home.hbs', {});
 });
 
-
-app.get('/api', cache('5 minutes'), function (request, response) {
+app.get('/api', cache('5 minutes'), function (request, response, next) {
   console.log('Generating new response');
-  axios.get('https://api.darksky.net/forecast/b9e88fa3742c42f79dfb5645bfd3891b/51.508742,-0.120850').then(function (r) {
+  axios.get(`https://api.darksky.net/forecast/b9e88fa3742c42f79dfb5645bfd3891b/${request.query.lat},${request.query.long}`).then(function (r) {
       response.json(r.data);
-  });
+  })
+  .catch(next);
 });
+
+//if you have a get request you need to use request.query.lat, if you want to use body you have to set up the body parser.
+
+app.get('/weather', function (request, response) {
+  response.render('weather.hbs', {})
+
+})
 
 app.listen(8000, function () {
   console.log('Listening on port 8000');
--- public/frontend.js
+++ public/frontend.js
@@ -1,6 +1,15 @@
-axios.get('/api')
- .then(function (response) {
-   console.log(response.data);
-   var t = document.getElementById('temp');
-   t.innerHTML = response.data.currently.temperature;
- });
+function get_weather () {
+  var lat = document.getElementById('lat').value;   //gets the value from the form by value
+  var long = document.getElementById('long').value;
+
+  axios.get('/api', {params: {lat: lat, long: long}}) //if you use request.query and post you need to use params...without it it's just {lat: lat, long: long}
+   .then(function (response) {
+     console.log(response.data);
+     var s = document.getElementById('icon');
+     s.innerHTML = response.data.currently.icon;
+     var t = document.getElementById('temp');
+     t.innerHTML = response.data.currently.temperature;
+   });
+
+  return false;
+}
